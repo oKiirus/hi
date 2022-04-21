801d3683288a8ce4c2462152231d908ac8035e84
oKiirus <77072551+oKiirus@users.noreply.github.com>
2022-04-20 20:49:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("/login.html")
+    return render_template("/index.html")
 
 @app.route("/index", methods = ["POST"])
 def login():
